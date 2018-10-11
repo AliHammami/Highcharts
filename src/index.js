05bbbc571eeb9fded6f59bb388edd197a2867a15
Philippe Candille <philippe@oclock.io>
2018-10-11 16:07:12 +0200

    Starter React et composant App

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('Hello');
+/**
+ * NPM import
+ */
+import '@babel/polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+
+/**
+ * Local import
+ */
+import App from 'src/components/App';
+
+/**
+ * Code
+ */
+
+const rootComponent = (
+  <App />
+);
+
+render(rootComponent, document.getElementById('root'));
